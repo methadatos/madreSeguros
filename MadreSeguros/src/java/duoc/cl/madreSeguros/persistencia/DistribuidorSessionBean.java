ad3b8320fc68b4dcc912446bd2b7046bdeed0873
Nicolas <Nicolas@192.168.0.25>
2016-11-17 23:20:13 -0300

    creacion de excepciones para las sessionbeans

--- MadreSeguros/src/java/duoc/cl/madreSeguros/persistencia/DistribuidorSessionBean.java
+++ MadreSeguros/src/java/duoc/cl/madreSeguros/persistencia/DistribuidorSessionBean.java
@@ -5,7 +5,14 @@
  */
 package duoc.cl.madreSeguros.persistencia;
 
+import duoc.cl.madreSeguros.dto.DistribuidorDTO;
+import duoc.cl.madreSeguros.entitys.Distribuidor;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -14,6 +21,63 @@ import javax.ejb.Stateless;
 @Stateless
 public class DistribuidorSessionBean {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceContext
+    private EntityManager em;
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    
+    private boolean agregarDistribuidor(DistribuidorDTO distribuidorIngresado){
+        Distribuidor infoDistribuidor=new Distribuidor();
+        
+        try {
+            infoDistribuidor.setRut(distribuidorIngresado.getRut());
+            infoDistribuidor.setNombre(distribuidorIngresado.getNombre());
+            infoDistribuidor.setNumeroContacto(distribuidorIngresado.getNumeroContacto());
+            infoDistribuidor.setCorreo(distribuidorIngresado.getCorreo());
+            em.persist(infoDistribuidor);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    public List<Distribuidor>listadoDistribuidor(){
+        return em.createNamedQuery("Distribuidor.findAll",Distribuidor.class)
+                .getResultList();
+    }
+    
+    public Distribuidor buscarDistribuidorXId(int idBuscado){
+        for(Distribuidor distribuidor:listadoDistribuidor()){
+            if(distribuidor.getIddistribuidor()==idBuscado){
+                return distribuidor;
+            }
+        }
+        return null;
+    }
+    
+    public boolean eliminarDistribuidor(int idDistribudiorEliminado){
+        for(Distribuidor distribuidor:listadoDistribuidor()){
+            if(distribuidor.getIddistribuidor()==idDistribudiorEliminado){
+                em.remove(distribuidor);
+            }
+        }
+        return false;
+    }
+    
+    public boolean updateDistribuidor(DistribuidorDTO distribuidorActualizado){
+        Distribuidor infoDistribuidor=new Distribuidor();
+        infoDistribuidor.setIddistribuidor(distribuidorActualizado.getIdDistribiudor());
+        infoDistribuidor.setRut(distribuidorActualizado.getRut());
+        infoDistribuidor.setNombre(distribuidorActualizado.getNombre());
+        infoDistribuidor.setNumeroContacto(distribuidorActualizado.getNumeroContacto());
+        infoDistribuidor.setCorreo(distribuidorActualizado.getCorreo());
+        for(Distribuidor distribuidor:listadoDistribuidor()){
+            if(distribuidor.equals(infoDistribuidor)){
+                em.createNamedQuery("");
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
 }
